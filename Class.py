19bfb5080e205d644cd8fded0d9311f7fc7f7dd5
k_0tik <dashachercashina509@gmail.com>
2026-05-15 14:06:54 +0300

    second task

--- Class.py
+++ Class.py
@@ -8,8 +8,30 @@ class Student:
         self.grades = {}
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in self.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Error'
 
 class Mentor:
+    def __init__ (self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+    
+
+class Lecturer(Mentor):
+    def __init__ (self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.grades = {}
+
+class Reviewer(Mentor):
     def __init__ (self, name, surname):
         self.name = name
         self.surname = surname
@@ -23,35 +45,35 @@ class Mentor:
         else:
             return 'Error'
 
-class Lecturer(Mentor):
-    def __init__ (self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-
-class Reviewer(Mentor):
-    def __init__ (self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor ('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
+student = Student('Алёхина', 'Ольга', 'Ж')
+ 
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+ 
+print(student.rate_lecture(lecturer, 'Python', 7))   # None
+print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
+print(student.rate_lecture(lecturer, 'C++', 8))      # Ошибка
+print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
+ 
+print(lecturer.grades)  # {'Python': [7]}  
 
-print(isinstance(lecturer, Mentor)) # True
-print(isinstance(reviewer, Mentor)) # True
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
 
-print(lecturer.courses_attached)    # []
-print(reviewer.courses_attached)    # []
+# cool_mentor = Mentor ('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
 
-print(best_student.grades)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+
+# print(isinstance(lecturer, Mentor)) # True
+# print(isinstance(reviewer, Mentor)) # True
+
+# print(lecturer.courses_attached)    # []
+# print(reviewer.courses_attached)    # []
+
+# print(best_student.grades)
