c13165ac0fd37542f63c409bf8b2c09fd785bc02
k_0tik <dashachercashina509@gmail.com>
2026-05-19 12:05:26 +0300

    changed str_method

--- Class.py
+++ Class.py
@@ -10,12 +10,25 @@ class Student:
         self.finished_courses.append(course_name)
     def rate_lecture(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
-            if course in self.grades:
+            if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
             return 'Error'
+    def average_score(self):
+        grades = []
+        for grades_list in self.grades.values():
+            grades.extend(grades_list)
+        if len(grades) == 0:
+            return 0
+        return round(sum(grades) / len(grades), 1)
+    def __str__(self):
+        return (f'Имя:{self.name}\n'
+                f'Фамилия:{self.surname}\n'
+                f'Средняя оценка за домашние задания:{self.average_score()}\n'
+                f'Курсы в процессе изучения:{self.courses_in_progress}\n'
+                f'Завершенные курсы:{self.finished_courses}')
 
 class Mentor:
     def __init__ (self, name, surname):
@@ -30,6 +43,15 @@ class Lecturer(Mentor):
         self.surname = surname
         self.courses_attached = []
         self.grades = {}
+    def average_score(self):
+        grades = []
+        for grades_list in self.grades.values():
+            grades.extend(grades_list)
+        if len(grades) == 0:
+            return 0
+        return round(sum(grades) / len(grades), 1)
+    def __str__(self):
+        return f'Имя:{self.name}\nФамилия:{self.surname}\nСредняя оценка за лекции:{self.average_score()}'
 
 class Reviewer(Mentor):
     def __init__ (self, name, surname):
@@ -44,10 +66,12 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Error'
+    def __str__(self):
+        return f'Имя:{self.name}\nФамилия:{self.surname}'
 
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
-student = Student('Алёхина', 'Ольга', 'Ж')
+student = Student('Ольга', 'Алёхина', 'Ж')
  
 student.courses_in_progress += ['Python', 'Java']
 lecturer.courses_attached += ['Python', 'C++']
@@ -58,7 +82,11 @@ print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
 print(student.rate_lecture(lecturer, 'C++', 8))      # Ошибка
 print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
  
-print(lecturer.grades)  # {'Python': [7]}  
+print(lecturer.grades)  # {'Python': [7]}
+
+print(reviewer)
+print(lecturer)
+print(student)
 
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
 # best_student.courses_in_progress += ['Python']
